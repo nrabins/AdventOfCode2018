701e550c47270ea5ec578d27ccd4d6ff353b8579
Nate Rabins <nrabins@alkamitech.com>
2018-12-11 10:13:26 -0600

    Day 11 part 2

--- AdventOfCode2018.Test/Day11_Test.cs
+++ AdventOfCode2018.Test/Day11_Test.cs
@@ -6,31 +6,60 @@ using System.Text;
 
 namespace AdventOfCode2018.Test
 {
-	public class Day11_Test
-	{
-		public Day11 Day11 { get; set; }
+    public class Day11_Test
+    {
+        public Day11 Day11 { get; set; }
 
-		[SetUp]
-		public void Setup()
-		{
-			Day11 = new Day11();
-		}
+        [SetUp]
+        public void Setup()
+        {
+            Day11 = new Day11();
+        }
 
-		[Test]
-		public void Part1()
-		{
-			var output = Day11.Part1(42);
-			Assert.AreEqual(Tuple.Create(21, 61), output);
-		}
+        [Test]
+        public void Part1()
+        {
+            var output = Day11.Part1(42);
+            Assert.AreEqual(Tuple.Create(21, 61), output);
+        }
 
-		[TestCase(4, 8, 3, 5)]
-		[TestCase(-5, 57, 122, 79)]
-		[TestCase(0, 39, 217, 196)]
-		[TestCase(4, 71, 101, 153)]
-		public void GetPowerAt(int expected, int serial, int x, int y)
-		{
-			var output = Day11.GetPowerAt(serial, x, y);
-			Assert.AreEqual(expected, output);
-		}
-	}
+        public class Part2TestCase
+        {
+            public Tuple<int,int,int> Expected { get; set; }
+            public int Serial { get; set; }
+
+            public Part2TestCase(Tuple<int, int, int> expected, int serial)
+            {
+                Expected = expected;
+                Serial = serial;
+            }
+        }
+
+        public static IEnumerable<Part2TestCase> Part2TestCases
+        {
+            get
+            {
+                yield return new Part2TestCase(Tuple.Create(90, 269, 16), 18);
+                yield return new Part2TestCase(Tuple.Create(232, 251, 12), 42);
+            }
+        }
+
+        [TestCaseSource(nameof(Part2TestCases))]
+        [Ignore("These tests take 12 minutes to run because I wrote tolerable-but-unoptimized code")]
+        public void Part2(Part2TestCase testCase)
+        {
+            var output = Day11.Part2(testCase.Serial);
+            Assert.AreEqual(testCase.Expected, output);
+        }
+
+        [TestCase(4, 8, 3, 5)]
+        [TestCase(-5, 57, 122, 79)]
+        [TestCase(0, 39, 217, 196)]
+        [TestCase(4, 71, 101, 153)]
+        public void GetPowerLevelAt(int expected, int serial, int x, int y)
+        {
+            var output = Day11.GetPowerLevelAt(serial, x, y);
+            Assert.AreEqual(expected, output);
+        }
+    }
 }
--- AdventOfCode2018/Days/Day11.cs
+++ AdventOfCode2018/Days/Day11.cs
@@ -8,73 +8,111 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2018.Days
 {
-	public class Day11 : AdventProblem2<string, int, Tuple<int, int>>
-	{
-		protected override string InputFilePath => "Inputs/Day11.txt";
+    public class Day11 : AdventProblem2<string, int, Tuple<int, int>, Tuple<int, int, int>>
+    {
+        protected override string InputFilePath => "Inputs/Day11.txt";
 
-		protected override int ParseInputFile()
-		{
-			return Parse(File.ReadAllText(InputFilePath));
-		}
+        protected override int ParseInputFile()
+        {
+            return Parse(File.ReadAllText(InputFilePath));
+        }
 
-		public override int Parse(string input)
-		{
-			return int.Parse(input);
-		}
+        public override int Parse(string input)
+        {
+            return int.Parse(input);
+        }
 
-		public override Tuple<int, int> Part1(int serial)
-		{
-			var powerLevels = new int[300, 300];
+        public override Tuple<int, int> Part1(int serial)
+        {
+            var powerLevels = GetPowerLevels(serial);
 
-			// fill out power level array
-			for (int x = 0; x < powerLevels.GetLength(0); x++)
-			{
-				for (int y = 0; y < powerLevels.GetLength(1); y++)
-				{
-					powerLevels[x, y] = GetPowerAt(serial, x + 1, y + 1);
-				}
-			}
+            // calculate the 3x3 square with the highest power level sum
+            var maxSum = int.MinValue;
+            Tuple<int, int> maxCoords = null;
+            for (var x = 0; x < powerLevels.GetLength(0) - 2; x++)
+            {
+                for (var y = 0; y < powerLevels.GetLength(1) - 2; y++)
+                {
+                    var sum = 0;
+                    for (var dx = 0; dx < 3; dx++)
+                    {
+                        for (var dy = 0; dy < 3; dy++)
+                        {
+                            sum += powerLevels[x + dx, y + dy];
+                        }
+                    }
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxCoords = Tuple.Create(x + 1, y + 1);
+                    }
+                }
+            }
+            return maxCoords;
+        }
 
-			// calculate the 3x3 square with the highest power level sum
-			var maxSum = int.MinValue;
-			Tuple<int, int> maxCoords = null;
-			for (int x = 0; x < powerLevels.GetLength(0) - 2; x++) 
-			{
-				for (int y = 0; y < powerLevels.GetLength(1) - 2; y++)
-				{
-					var sum = 0;
-					for (var dx = 0; dx < 3; dx++)
-					{
-						for (var dy = 0; dy < 3; dy++)
-						{
-							sum += powerLevels[x + dx, y + dy];
-						}
-					}
-					if (sum > maxSum)
-					{
-						maxSum = sum;
-						maxCoords = Tuple.Create(x + 1, y + 1);
-					}
-				}
-			}
+        public override Tuple<int, int, int> Part2(int serial)
+        {
+            var powerLevels = GetPowerLevels(serial);
 
-			return maxCoords;
-		}
+            // calculate the 3x3 square with the highest power level sum
+            var maxSum = int.MinValue;
+            Tuple<int, int, int> maxCoords = null;
 
-		public override Tuple<int, int> Part2(int parsed)
-		{
-			throw new NotImplementedException();
-		}
+            for (var size = 1; size <= powerLevels.GetLength(0); size++)
+            {
+                for (var x = 0; x < powerLevels.GetLength(0) + 1 - size; x++)
+                {
+                    for (var y = 0; y < powerLevels.GetLength(1) + 1 - size; y++)
+                    {
+                        var sum = 0;
+                        for (var dx = 0; dx < size; dx++)
+                        {
+                            for (var dy = 0; dy < size; dy++)
+                            {
+                                sum += powerLevels[x + dx, y + dy];
+                            }
+                        }
+                        if (sum > maxSum)
+                        {
+                            maxSum = sum;
+                            maxCoords = Tuple.Create(x + 1, y + 1, size);
+                        }
+                    }
+                }
+                Console.WriteLine($"  Size {size}: max sum = {maxSum} at {maxCoords}");
+            }
+            
+            return maxCoords;
+        }
 
-		public int GetPowerAt(int serial, int x, int y)
-		{
-			var rackId = x + 10;
-			var powerLevel = rackId * y;
-			powerLevel += serial;
-			powerLevel *= rackId;
-			powerLevel = MathUtils.GetHundredsDigit(powerLevel);
-			powerLevel -= 5;
-			return powerLevel;
-		}
-	}
+
+
+        private int[,] GetPowerLevels(int serial)
+        {
+            var powerLevels = new int[300, 300];
+
+            // fill out power level array
+            for (int x = 0; x < powerLevels.GetLength(0); x++)
+            {
+                for (int y = 0; y < powerLevels.GetLength(1); y++)
+                {
+                    powerLevels[x, y] = GetPowerLevelAt(serial, x + 1, y + 1);
+                }
+            }
+
+            return powerLevels;
+        }
+
+        public int GetPowerLevelAt(int serial, int x, int y)
+        {
+            var rackId = x + 10;
+            var powerLevel = rackId * y;
+            powerLevel += serial;
+            powerLevel *= rackId;
+            powerLevel = MathUtils.GetHundredsDigit(powerLevel);
+            powerLevel -= 5;
+            return powerLevel;
+        }
+    }
 }
